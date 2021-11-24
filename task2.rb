ecf1580186604f387bbdf0066c287f3f0d0e1fc8
Evgeny Skaletskii <evgeny.skaletskii@jetruby.com>
2021-11-24 10:47:03 +0300

    fixed greeting

--- task2.rb
+++ task2.rb
@@ -8,11 +8,9 @@ def greeting
     print "Введите свой возраст >"
     age = gets.chomp.to_i
 
-    if age > 0 && age <= 18
+    if age <= 18
         puts "Привет, #{name} #{surname}. Тебе меньше 18 лет, но начать учиться программировать никогда не рано"
-    elsif age > 18 && age < 120
-        puts "Привет, #{name} #{surname}. Самое время заняться делом!"
     else
-        puts "С вашим возрастом что-то не то..."
+        puts "Привет, #{name} #{surname}. Самое время заняться делом!"
     end
 end
--- task2_spec.rb
+++ task2_spec.rb
@@ -2,24 +2,17 @@ require 'rspec'
 require './task2.rb'
 
 RSpec.describe "Tests for taks2" do
-    it "#age is less than 18" do
+    it "#greeting -> age is less than 18" do
         allow_any_instance_of(Kernel).to receive(:gets).and_return('Иван', 'Иванов', '17')
         expect do
             greeting
         end.to output("Введите своё имя >Введите свою фамилию >Введите свой возраст >Привет, Иван Иванов. Тебе меньше 18 лет, но начать учиться программировать никогда не рано\n").to_stdout
     end
 
-    it "#age is greater than 18" do
+    it "#greeting -> age is greater than 18" do
         allow_any_instance_of(Kernel).to receive(:gets).and_return('Иван', 'Иванов', '20')
         expect do
              greeting 
         end.to output("Введите своё имя >Введите свою фамилию >Введите свой возраст >Привет, Иван Иванов. Самое время заняться делом!\n").to_stdout
-    end
-
-    it "#incorrect age" do
-        allow_any_instance_of(Kernel).to receive(:gets).and_return('Иван', 'Иванов', 'azaza')
-        expect do
-            greeting 
-        end.to output("Введите своё имя >Введите свою фамилию >Введите свой возраст >С вашим возрастом что-то не то...\n").to_stdout
     end
 end
