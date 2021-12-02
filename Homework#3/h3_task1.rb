3a74ed7af70ea3c6d51f7c9de01542dc40ffac57
Evgeny Skaletskii <evgeny.skaletskii@jetruby.com>
2021-12-02 22:02:14 +0300

    Fix task1 with 'ends_with?' method

--- Homework#3/h3_task1.rb
+++ Homework#3/h3_task1.rb
@@ -2,7 +2,7 @@ def process_user_input
     puts "Введите слово >"
     word = gets.chomp
 
-    if word.downcase.include?('cs')
+    if word.downcase.end_with?('cs')
         puts 2 ** word.length
     else
         puts word.reverse
--- Homework#3/h3_task1_tests.rb
+++ Homework#3/h3_task1_tests.rb
@@ -3,37 +3,22 @@ require './h3_task1.rb'
 
 RSpec.describe "Test user word" do
     it "contains 'cs' in lowercase" do
-        allow_any_instance_of(Kernel).to receive(:gets).and_return('css')
-        expect do
-            process_user_input
-        end.to output("Введите слово >\n8\n").to_stdout
+        allow_any_instance_of(Kernel).to receive(:gets).and_return('abbacs')
+        expect { process_user_input }.to output("Введите слово >\n64\n").to_stdout
     end
 
     it "contains 'cs' in uppercase" do
         allow_any_instance_of(Kernel).to receive(:gets).and_return('someCS')
-        expect do
-            process_user_input
-        end.to output("Введите слово >\n64\n").to_stdout
-    end
-
-    it "multiple words with 'cs'" do
-        allow_any_instance_of(Kernel).to receive(:gets).and_return('css and')
-        expect do
-            process_user_input
-        end.to output("Введите слово >\n128\n").to_stdout
+        expect { process_user_input }.to output("Введите слово >\n64\n").to_stdout
     end
 
     it "doesn't contain 'cs'" do
         allow_any_instance_of(Kernel).to receive(:gets).and_return('some')
-        expect do
-            process_user_input
-        end.to output("Введите слово >\nemos\n").to_stdout
+        expect { process_user_input }.to output("Введите слово >\nemos\n").to_stdout
     end
 
     it "word less than 2 characters" do
         allow_any_instance_of(Kernel).to receive(:gets).and_return('c')
-        expect do
-            process_user_input
-        end.to output("Введите слово >\nc\n").to_stdout
+        expect { process_user_input }.to output("Введите слово >\nc\n").to_stdout
     end
 end
