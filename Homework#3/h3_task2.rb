043dadc54668332414200da57af334107af249d8
Evgeny Skaletskii <evgeny.skaletskii@jetruby.com>
2021-12-02 10:35:49 +0300

    Create pokemons methods

---
+++ Homework#3/h3_task2.rb
@@ -0,0 +1,32 @@
+def setup_pokemons(number)
+    pokemons = []
+    number.times do 
+        pokemon = get_next_pokemon
+        pokemons.push(pokemon)
+    end
+    pokemons
+end
+
+def get_next_pokemon
+    puts "Введите имя покемона >"
+    name = gets.chomp
+
+    puts "Введите цвет покемона >"
+    color = gets.chomp
+
+    {name: name, color: color}
+end
+
+def show_pokemons(pokemons)
+    puts pokemons.to_s
+end
+
+def pokemons_game
+    puts "Введите число покемонов >"
+    pokemons_number = gets.chomp.to_i
+
+    pokemons = setup_pokemons(pokemons_number)
+    show_pokemons(pokemons)
+end
+
+
